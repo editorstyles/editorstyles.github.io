7cf1222c19c39492474d5a6c871050b7f62db3a6
Kite <ixkaito@gmail.com>
2016-07-20 02:49:10 +0900

    Prepare to develop

---
+++ frasco.config.js
@@ -0,0 +1,48 @@
+module.exports = {
+  port: 4000,
+
+  tasks: {
+    imagemin:   true,
+    sass:       true,
+    server:     true,
+    webpack:    true,
+  },
+
+  paths: {
+    dest:      "_site",
+    posts:     "_posts",
+    assets:    "./",
+    sass:      "sass",
+    css:       "css",
+    jsSrc:     "assets/_js",
+    js:        "assets/js",
+    imagesSrc: "assets/_images",
+    images:    "assets/images",
+  },
+
+  jekyll: {
+    config: {
+      default:     "_config.yml",
+      development: "_config_development.yml",
+      production:  "",
+    }
+  },
+
+  sass: {
+    outputStyle: "expanded",
+  },
+
+  autoprefixer: {
+    browsers: [
+      "> 1%",
+      "last 2 versions",
+      "Firefox ESR",
+    ]
+  },
+
+  js: {
+    entry: [
+      "main.js",
+    ],
+  },
+}
